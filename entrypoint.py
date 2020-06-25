d5907cb4a3030bc415043a31865cf7a798e7ad44
Daniel Llewellyn <diddledan@ubuntu.com>
2020-06-26 00:24:10 +0100

    Add shell escaping of commandline
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- entrypoint.py
+++ entrypoint.py
@@ -2,10 +2,11 @@
 
 import os
 import sys
-import time
+import shlex
 import subprocess
 
 if __name__ == "__main__":
+        commandline = " ".join([shlex.quote(a) for a in sys.argv[1:]])
         cmd = open("/docker-commandline.sh", "w")
         cmd.write("""#!/bin/bash
 /bin/systemctl start snapd.service snapd.socket
@@ -16,7 +17,7 @@ cd "{wd}"
 {commandline}
 
 /bin/systemctl exit $?
-""".format(wd=os.getcwd(), commandline=" ".join(sys.argv[1:])))
+""".format(wd=os.getcwd(), commandline=commandline))
         cmd.close()
         os.chmod("/docker-commandline.sh", 0o755)
         os.execvp("/lib/systemd/systemd", ["/lib/systemd/systemd", "--system"])
